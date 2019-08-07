6748801224aa61bb665e8ceaccb9c9ac23565bfc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-07 20:17:16 +0000

    Deploy JulienUnip/Reactjs-travis to github.com/JulienUnip/Reactjs-travis.git:gh-pages

---
+++ precache-manifest.64a6a0d6031c34b78c1c1809a1c40fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5677d2809d2e1d875537aa650284a868",
+    "url": "/reactjs-travis/index.html"
+  },
+  {
+    "revision": "8748e3be052308f191c0",
+    "url": "/reactjs-travis/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactjs-travis/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8748e3be052308f191c0",
+    "url": "/reactjs-travis/static/js/main.6fe7a364.chunk.js"
+  },
+  {
+    "revision": "91c61fd3eec3fe4d8a28",
+    "url": "/reactjs-travis/static/js/runtime~main.b3d43d7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-travis/static/media/logo.5d5d9eef.svg"
+  }
+]);
